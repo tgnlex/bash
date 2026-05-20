be891d0a9c8e6e1c73c4725029fdc3ac3efdaedf
root <root@void.lan>
2026-05-19 21:24:52 -0400

    auto-commit

---
+++ filetype.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+if [ $# -ne 2 ]; then
+  echo "usage $0 [directory] [new_extension]"
+  exit 1
+fi 
+
+DIR="$1"
+EXT="$2"
+
+for file in "$DIR"/*; do 
+  if [ -f "$file" ]; then 
+    mv -- "$file" "${file%.*}.$EXT"
+  fi 
+done
